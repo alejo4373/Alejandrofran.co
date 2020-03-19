bdbd783917a17118d2085c8046310c17be3178ec
Alejo <alejandrofranco@pursuit.org>
2020-03-19 13:02:39 -0400

    change CLI to have straight corners

--- src/Logos/CLI.jsx
+++ src/Logos/CLI.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 const CLI = ({ className, id }) => {
   return (
     <svg className={className} id={id} viewBox="0 0 65 65" >
-      <g transform="translate(-15.5,-15.5)">
-        <path
-          d="m21.4 18.5c-3.3 0-5.9 2.7-5.9 5.9v47.3c0 3.2 2.6 5.9 5.9 5.9h53.2c3.3 0 5.9-2.7 5.9-5.9v-47.3c0-3.2-2.6-5.9-5.9-5.9h-26.6z" fill="#a5a5a5" />
-        <path d="m21.4 18.5c-3.3 0-5.9 2.6-5.9 5.9v44.6c0 3.3 2.6 5.9 5.9 5.9h53.2c3.3 0 5.9-2.6 5.9-5.9v-44.7c0-3.3-2.6-5.9-5.9-5.9h-53.2z" fill="#000" />
-        <path d="m24.4 27.7v3.3l8.9 5.2-8.9 5.2v3.3l14.8-8.5zm14.7 14.4v3h14.8v-3z" fill="#ecf0f1" /></g></svg>
+      <rect width="65" height="65" />
+      <path
+        transform="translate(-15.5,10)"
+        d="m24.427.7v3.3l8.9 5.2-8.9 5.2v3.3l14.8-8.5zm14.7 14.4v3h14.8v-3z"
+        fill="#ecf0f1" />
+    </svg >
   )
 }
 
