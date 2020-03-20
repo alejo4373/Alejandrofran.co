29b30d293ef3d13f5b3316e72114354513027761
Alejo <alejandrofranco@pursuit.org>
2020-03-20 13:20:49 -0400

    mvp contact form

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
-function ContactMe() {
+const ContactMe = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [formSent, setFormSent] = useState(false);
+  const [formSuccess, setFormSuccess] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const url = 'https://formspree.io/mrglavjj'
+    try {
+      const { data } = await axios.post(url, { name, email, message })
+      console.log(data)
+      setFormSent(true)
+      setFormSuccess(true)
+    } catch (err) {
+      console.err(err)
+      setFormSent(true)
+      setFormSuccess(false)
+    }
+  }
+
   return (
     <div className="contact-me section black">
       <h2 className="title">Contact Me</h2>
+      {formSent && formSuccess
+        ? (
+          <p>Form Sent Successfully</p>
+        )
+        : (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Your Name</label>
+            <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message" value={message} onChange={(e) => setMessage(e.target.value)} />
+            <button>Send</button>
+          </form>
+        )
+      }
     </div>
   )
 }
