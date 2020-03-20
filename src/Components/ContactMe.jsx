416320efbf3d5788bee3fb360b875543bc1313a3
Alejo <alejandrofranco@pursuit.org>
2020-03-20 15:00:01 -0400

    style contact form

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import '../Styles/ContactMe.css'
 
 const ContactMe = () => {
   const [name, setName] = useState('');
@@ -26,42 +27,53 @@ const ContactMe = () => {
   return (
     <div className="contact-me section black">
       <h2 className="title">Contact Me</h2>
-      {formSent && formSuccess
-        ? (
-          <p>Form Sent Successfully</p>
-        )
-        : (
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Your Name</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <label htmlFor="message">Message</label>
-            <textarea
-              name="message"
-              id="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              required
-            />
-            <button>Send</button>
-          </form>
-        )
-      }
+      <div className="content">
+        {formSent && formSuccess
+          ? (
+            <div className='thankyou-card'>
+              <p>Thank you! I will get back to you shortly</p>
+            </div>
+          )
+          : (
+            <form onSubmit={handleSubmit}>
+              <fieldset>
+                <label htmlFor="name">Your Name:</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </fieldset>
+              <fieldset>
+                <label htmlFor="email">Email:</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </fieldset>
+              <fieldset>
+                <label htmlFor="message">Message:</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  required
+                  rows="14"
+                />
+              </fieldset>
+              <button>Send</button>
+            </form>
+          )
+        }
+      </div>
     </div>
   )
 }
