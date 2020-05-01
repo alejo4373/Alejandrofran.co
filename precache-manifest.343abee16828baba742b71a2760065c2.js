db19136ca43e2c68e6978b978e674a574d16faf0
alejo4373 <alejandrofranco@pursuit.org>
2020-05-01 14:59:33 -0400

    Updates

---
+++ precache-manifest.343abee16828baba742b71a2760065c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9175380bc5272a6b9c5a0c38d369f0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e92a6c8355761847fd03",
+    "url": "/static/css/main.1b92ab72.chunk.css"
+  },
+  {
+    "revision": "8f2b31efab816babf5f6",
+    "url": "/static/js/2.a1e3089a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a1e3089a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e92a6c8355761847fd03",
+    "url": "/static/js/main.602d25ff.chunk.js"
+  },
+  {
+    "revision": "8c658ede3fae7933a7b2",
+    "url": "/static/js/runtime-main.da70dfce.js"
+  }
+]);
