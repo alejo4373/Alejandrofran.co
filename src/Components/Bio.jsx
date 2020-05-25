b8dde63d49bb3ad0df3fe48a25502ef271a99800
alejo4373 <alejandrofranco@pursuit.org>
2020-05-24 20:31:16 -0400

    fix wording in bio section

--- src/Components/Bio.jsx
+++ src/Components/Bio.jsx
@@ -7,11 +7,11 @@ function Bio() {
         <h2 className="title"> I </h2>
 
         <p>
-          Was interested in computers since a young age. The interest became a passion when I was accepted into the Google-Funded Software Engineering Fellowship Pursuit Core 4.1, in late 2017. During my time as a Fellow, I technically led the UltraVox team to be winners at Pursuit’s 2018 Web Hackathon by making NYC bus times available to the visually impaired with an App that leveraged voice to speech and speech to text web technologies.
+          Was interested in computers since a young age. The interest became a passion when I was accepted into the Google-Funded Software Engineering Fellowship at Pursuit in late 2017. During my time as a Fellow, I technically led the UltraVox team to be winners at Pursuit’s 2018 Web Hackathon by making NYC bus times available to the visually impaired with an App that leveraged speech to text and text to speech web technologies.
         </p>
         <br />
         <p>
-          My hard work, passion for technology and eagerness to help others, coupled with my technical acuteness earned me being invited to become a Teaching Assistant for the 5.1 Pursuit Core Class. Now I lead as a Full Stack Web Development instructor the 6.2 class at Pursuit. My class is scheduled to graduate June 30th 2020.
+          My hard work, passion for technology and eagerness to help others, coupled with my technical acuteness earned me being invited to become a Teaching Assistant for the 5.1 Pursuit Core Class. Now I lead as a Full Stack Web Development instructor the 6.2 Web Class at Pursuit. My class is scheduled to graduate June 30th 2020.
         </p>
       </div>
     </div>
